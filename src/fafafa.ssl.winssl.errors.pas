24359e1e4a7cf681b614d2020dca50f118a225d2
dtamade <dtamade@gmail.com>
2025-12-29 11:56:56 +0800

    feat(errors): P2 完善 OpenSSL/WinSSL 错误码映射
    - WinSSL: 添加 ClassifyWinSSLError 函数映射到 TSSLErrorCode
    - WinSSL: 添加 GetWinSSLErrorCategory 获取错误分类名称
    - 与 OpenSSL ClassifyOpenSSLError 保持一致的分类逻辑
    - 支持 SSPI/CERT/TRUST/WIN32 错误分类

--- src/fafafa.ssl.winssl.errors.pas
+++ src/fafafa.ssl.winssl.errors.pas
@@ -7,6 +7,7 @@ interface
 
 uses
   SysUtils, Classes, Windows,
+  fafafa.ssl.base,           // P2: TSSLErrorCode 统一错误码
   fafafa.ssl.winssl.base;
 
 type
@@ -85,6 +86,12 @@ procedure SetGlobalErrorHandler(AHandler: ISSLErrorHandler);
 
 { 启用/禁用错误日志 }
 procedure EnableErrorLogging(AEnabled: Boolean);
+
+{ P2: WinSSL 错误码到 TSSLErrorCode 的映射 }
+function ClassifyWinSSLError(AErrorCode: DWORD): TSSLErrorCode;
+
+{ P2: 获取 WinSSL 错误分类名称 }
+function GetWinSSLErrorCategory(AErrorCode: DWORD): string;
 
 implementation
 
@@ -334,6 +341,126 @@ end;
 procedure EnableErrorLogging(AEnabled: Boolean);
 begin
   GErrorLoggingEnabled := AEnabled;
+end;
+
+{ P2: WinSSL 错误码到 TSSLErrorCode 的映射
+  与 OpenSSL ClassifyOpenSSLError 保持一致的分类逻辑 }
+function ClassifyWinSSLError(AErrorCode: DWORD): TSSLErrorCode;
+begin
+  case LONG(AErrorCode) of
+    // 成功状态
+    LONG(SEC_E_OK):
+      Result := sslErrNone;
+
+    // 握手/协议错误
+    LONG(SEC_I_CONTINUE_NEEDED),
+    LONG(SEC_I_COMPLETE_NEEDED),
+    LONG(SEC_I_COMPLETE_AND_CONTINUE):
+      Result := sslErrNone;  // 这些是正常的握手状态
+
+    LONG(SEC_E_INCOMPLETE_MESSAGE):
+      Result := sslErrWouldBlock;
+
+    LONG(SEC_E_INVALID_TOKEN),
+    LONG(SEC_E_MESSAGE_ALTERED),
+    LONG(SEC_E_OUT_OF_SEQUENCE):
+      Result := sslErrProtocol;
+
+    LONG(SEC_E_ALGORITHM_MISMATCH),
+    LONG(SEC_E_UNSUPPORTED_FUNCTION):
+      Result := sslErrUnsupported;
+
+    // 证书错误
+    LONG(SEC_E_UNTRUSTED_ROOT),
+    LONG(CERT_E_UNTRUSTEDROOT):
+      Result := sslErrCertificateUntrusted;
+
+    LONG(SEC_E_CERT_EXPIRED),
+    LONG(CERT_E_EXPIRED):
+      Result := sslErrCertificateExpired;
+
+    LONG(CERT_E_REVOKED):
+      Result := sslErrCertificateRevoked;
+
+    LONG(CERT_E_REVOCATION_FAILURE):
+      Result := sslErrVerificationFailed;
+
+    LONG(CERT_E_CN_NO_MATCH),
+    LONG(CERT_E_INVALID_NAME):
+      Result := sslErrHostnameMismatch;
+
+    LONG(CERT_E_WRONG_USAGE):
+      Result := sslErrCertificate;
+
+    LONG(TRUST_E_CERT_SIGNATURE):
+      Result := sslErrVerificationFailed;
+
+    LONG(SEC_E_CERT_UNKNOWN),
+    LONG(SEC_E_WRONG_PRINCIPAL):
+      Result := sslErrCertificate;
+
+    // 句柄/参数错误
+    LONG(SEC_E_INVALID_HANDLE),
+    LONG(ERROR_INVALID_HANDLE):
+      Result := sslErrInvalidParam;
+
+    LONG(ERROR_INVALID_PARAMETER),
+    LONG(SEC_E_INVALID_PARAMETER):
+      Result := sslErrInvalidParam;
+
+    // 内存错误
+    LONG(ERROR_NOT_ENOUGH_MEMORY),
+    LONG(SEC_E_INSUFFICIENT_MEMORY):
+      Result := sslErrMemory;
+
+    // 初始化错误
+    LONG(SEC_E_SECPKG_NOT_FOUND),
+    LONG(SEC_E_NOT_OWNER):
+      Result := sslErrNotInitialized;
+
+    // I/O 错误
+    LONG(SEC_E_CONTEXT_EXPIRED):
+      Result := sslErrConnection;
+
+    // 访问错误
+    LONG(ERROR_ACCESS_DENIED),
+    LONG(SEC_E_LOGON_DENIED):
+      Result := sslErrConfiguration;
+
+    // 不支持
+    LONG(ERROR_NOT_SUPPORTED):
+      Result := sslErrUnsupported;
+
+  else
+    // 未知错误映射到通用错误
+    Result := sslErrOther;
+  end;
+end;
+
+{ P2: 获取 WinSSL 错误分类名称 }
+function GetWinSSLErrorCategory(AErrorCode: DWORD): string;
+begin
+  // 根据错误码范围判断分类
+  case LONG(AErrorCode) of
+    // SEC_E_* 和 SEC_I_* 错误 (0x80090000 - 0x8009FFFF)
+    LONG($80090000)..LONG($8009FFFF):
+      Result := 'SSPI';
+
+    // CERT_E_* 错误 (0x800B0100 - 0x800B01FF)
+    LONG($800B0100)..LONG($800B01FF):
+      Result := 'CERT';
+
+    // TRUST_E_* 错误 (0x800B0000 - 0x800B00FF)
+    LONG($800B0000)..LONG($800B00FF):
+      Result := 'TRUST';
+
+    // Windows 系统错误
+    0..65535:
+      Result := 'WIN32';
+
+  else
+    Result := 'UNKNOWN';
+  end;
 end;
 
 initialization
